4a751dc26269b7c0e457f87a3779ba0c276dabbe
Federico Giacon <58218759+fedgiac@users.noreply.github.com>
2025-06-24 10:01:14 +0000

    Support RPCs with maximum block range

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,48 @@ interface AppDataInfo {
   count: number;
 }
 
+// Fixme: script assumes mainnet block time
+const BLOCK_TIME = 12;
+const DAYS_TO_FETCH = 30;
+const ETH_FLOW_ABI = [
+  // https://github.com/cowprotocol/ethflowcontract/blob/7166070efa65df4282e9f14e54beef57619ad883/src/interfaces/ICoWSwapOnchainOrders.sol#L23-L44
+  "event OrderPlacement(address indexed sender, tuple(address, address, address, uint256, uint256, uint32, bytes32, uint256, bytes32, bool, bytes32, bytes32) order, tuple(uint8 scheme, bytes data) signature, bytes data)",
+];
+// Most RPCs limit the number of blocks you can query simultaneously for events.
+// Decreasing this numbers makes more RPCs compatible with this script, on the
+// other hand it makes its running time longer.
+const MAX_BLOCK_RANGE = 10000;
+
+interface OrderPlacementEventsInput {
+  daysToFetch: number;
+  ethFlowAddress: string;
+  provider: ethers.JsonRpcApiProvider;
+}
+async function orderPlacementEvents(
+  { daysToFetch, ethFlowAddress, provider }: OrderPlacementEventsInput,
+): Promise<(ethers.Log | ethers.EventLog)[]> {
+  const ethFlow = new Contract(ethFlowAddress, ETH_FLOW_ABI).connect(provider);
+  const orderPlacementFilter = ethFlow.filters.OrderPlacement;
+  const currentBlock = await provider.getBlockNumber();
+  const blocksToFetch = daysToFetch * 24 * 3600 / BLOCK_TIME;
+
+  const events = [];
+  const queryCount = Math.ceil(blocksToFetch / MAX_BLOCK_RANGE);
+  for (let i = 0; i < queryCount; i++) {
+    const startingBlock = currentBlock -
+      Math.min(blocksToFetch, (i + 1) * MAX_BLOCK_RANGE);
+    const endingBlock = currentBlock - i * MAX_BLOCK_RANGE;
+    //console.debug({ startingBlock, endingBlock });
+    const partial = await ethFlow.queryFilter(
+      orderPlacementFilter,
+      startingBlock,
+      endingBlock,
+    );
+    events.push(...partial);
+  }
+  return events;
+}
+
 const params = parseArgs(Deno.args, {
   string: ["address", "network"],
 });
@@ -33,24 +75,12 @@ const ethFlowAddress = ethers.getAddress(params.address);
 const rpcUrl = getRpcUrl(network);
 const provider = new ethers.JsonRpcProvider(rpcUrl);
 
-const ethFlowAbi = [
-  // https://github.com/cowprotocol/ethflowcontract/blob/7166070efa65df4282e9f14e54beef57619ad883/src/interfaces/ICoWSwapOnchainOrders.sol#L23-L44
-  "event OrderPlacement(address indexed sender, tuple(address, address, address, uint256, uint256, uint32, bytes32, uint256, bytes32, bool, bytes32, bytes32) order, tuple(uint8 scheme, bytes data) signature, bytes data)",
-];
-const ethFlow = new Contract(ethFlowAddress, ethFlowAbi).connect(provider);
-
-const BLOCK_TIME = 12;
-const DAYS_TO_FETCH = 30;
-const BLOCKS_TO_FETCH = DAYS_TO_FETCH * 24 * 3600 / BLOCK_TIME;
-const CURRENT_BLOCK = await provider.getBlockNumber();
-const orderPlacementFilter = ethFlow.filters.OrderPlacement;
-const orderPlacementEvents = await ethFlow.queryFilter(
-  orderPlacementFilter,
-  CURRENT_BLOCK - BLOCKS_TO_FETCH,
-  CURRENT_BLOCK,
-);
-
-const appDataFromEvents = orderPlacementEvents.map((e) => {
+const events = await orderPlacementEvents({
+  daysToFetch: DAYS_TO_FETCH,
+  provider,
+  ethFlowAddress,
+});
+const appDataFromEvents = events.map((e) => {
   if (!("args" in e)) {
     throw new Error(
       `Failed to decode event at transaction ${e.transactionHash}`,
@@ -99,7 +129,7 @@ async function getAppCode(
 }
 
 console.log(
-  `In the last ${DAYS_TO_FETCH} days there were ${orderPlacementEvents.length} eth-flow orders for contract ${ethFlowAddress} on network ${network}.`,
+  `In the last ${DAYS_TO_FETCH} days there were ${events.length} eth-flow orders for contract ${ethFlowAddress} on network ${network}.`,
 );
 console.log(
   "The following is a list of all the app data used in these orders. When possible, the corresponding `appCode` field has been recovered.",
